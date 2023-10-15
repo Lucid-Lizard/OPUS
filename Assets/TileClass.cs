b53e3e08b2277290b4fb1e93b40a1790b65c5427
Lucid Lizard <danielrpankratz@gmail.com>
2023-10-15 18:20:25 -0500

    Tile Updates
    Made leaves AutoTile, and removed Tile Texture from TileClass

--- Assets/TileClass.cs
+++ Assets/TileClass.cs
@@ -7,7 +7,6 @@ public class TileClass : ScriptableObject
 {
     public string tileName;
     public Sprite tileSprite;
-    public Texture2D tileTexture;
     public bool AutoTile = false;
     [Header("Sublings")]
     public List<string> siblings = new List<string>();
