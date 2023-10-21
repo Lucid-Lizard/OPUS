40c6de48116443d88b94d9ce948924efdfe832a7
SpaceyJopl <jabinpankratz@gmail.com>
2023-10-21 16:28:54 -0500

    Added placing blocks + Cursor

--- Assets/TilemapEditor.cs
+++ Assets/TilemapEditor.cs
@@ -5,15 +5,21 @@ public class TilemapEditor : MonoBehaviour
 {
     public Tilemap tilemap;
     public Camera cam;
-
+    public TileBase Selected = null;
     public Vector3Int MousePos;
+    public GameObject Cursor;
     private void Update()
     {
         MousePos = new Vector3Int(Mathf.FloorToInt(cam.ScreenToWorldPoint(Input.mousePosition).x), Mathf.FloorToInt(cam.ScreenToWorldPoint(Input.mousePosition).y), 0);
+        Cursor.transform.position = MousePos + (Vector3.one / 2);
         if(Input.GetMouseButtonDown(0))
         {
             
-            tilemap.SetTile(MousePos, null);
+            tilemap.SetTile(MousePos, Selected);
+        } else if (Input.GetMouseButtonDown(1))
+        {
+            Selected = tilemap.GetTile(MousePos);
+            
         }
     }
 }
