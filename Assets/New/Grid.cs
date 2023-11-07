5eca1ad62de1191d7fdc7fad9d7a30fb7b0474bb
Lucid Lizard <danielrpankratz@gmail.com>
2023-11-06 21:38:16 -0600

    MESH!!!
    you have to load the scene called newgame in the scenes folder for it to work but yeah its real!!!

--- Assets/New/Grid.cs
+++ Assets/New/Grid.cs
@@ -20,12 +20,22 @@ public class Grid : MonoBehaviour
 
     int seed;
 
+    public Sprite Dirt;
+    public Sprite Stone;
+    public Sprite Grass;
+
+    public float numywumy = 8f;
+
+
+
     private void Start()
     {
+
         seed = Random.Range(-10000, 10000);
         CellularAutomata();
 
     }
+
 
     void CellularAutomata()
     {
@@ -42,20 +52,30 @@ public class Grid : MonoBehaviour
 
     public void GenerateMap()
     {
-
+        
         //TileClass tile;
         for (int x = 0; x < worldSize.x; x++)
         {
-            //float height = Mathf.PerlinNoise((x + seed) * GetCurrentBiome(x, heightAddition).terrainFreq, seed * GetCurrentBiome(x, heightAddition).terrainFreq) * GetCurrentBiome(x, heightAddition).heightMultiplier + heightAddition;
-            for (int y = 0; y < worldSize.y; y++)
+            float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
+
+            for (int y = 0; y < height; y++)
             {
+                int threshold = 50;
+                if(y < (worldSize.y / 5) * 3)
+                {
+                    threshold = 50;
+                } else if (y < (worldSize.y / 5) * 4)
+                {
+                    threshold = 20;
+                } else
+                {
+                    threshold = 5;
+                }
 
-
-                if (Random.Range(0, 100) > 50 - y / 10)
+                if (Random.Range(0, 100) > threshold)
                     map[x, y] = 1;
                 else
                     map[x, y] = 0;
-
 
 
             }
@@ -115,7 +135,6 @@ public class Grid : MonoBehaviour
 
     public void PlaceMap(int[,] map)
     {
-        map[0, 0] = 1;
         UnityEngine.Mesh mesh = new UnityEngine.Mesh();
         List<Vector3> vertices = new List<Vector3>();
         List<int> triangles = new List<int>();
@@ -125,25 +144,39 @@ public class Grid : MonoBehaviour
             for (int y = 0; y < worldSize.y; y++)
             {
                 int cell = map[x, y];
-                if(cell == 1)
+                if (cell == 1)
                 {
-                    Vector3 a = new Vector3(x - 0.5f, y + .5f,0);
-                    Vector3 b = new Vector3(x + 0.5f, y + .5f, 0);
-                    Vector2 c = new Vector3(x - 0.5f, y - .5f, 0);
-                    Vector3 d = new Vector3(x + 0.5f, y - .5f, 0);
-                    Vector3[] v = new Vector3[] { a, b, c, b, d, c };
+                    
+                    Vector3 a = new Vector3(x - 0.5f, y + 0.5f, 0);
+                    Vector3 b = new Vector3(x + 0.5f, y + 0.5f, 0);
+                    Vector3 c = new Vector3(x - 0.5f, y - 0.5f, 0);
+                    Vector3 d = new Vector3(x + 0.5f, y - 0.5f, 0);
+                    Vector3[] v = new Vector3[] { a, b, c, d };
 
-                    Vector2 v1 = new Vector2(0, 0);
-                    Vector2 v2 = new Vector2(0, 1);
-                    Vector2 v3 = new Vector2(1, 1);
-                    Vector2 v4 = new Vector2(1, 0);
-                    Vector2[] u = new Vector2[] { v1, v2, v3, v2, v4, v3 };
-                    for (int k = 0; k < 6; k++)
-                    {
-                        vertices.Add(v[k]);
-                        uv.Add(u[k]);
-                        triangles.Add(triangles.Count);
-                    }
+                    /*Vector2 v1 = new Vector2((x + 1) / (float)worldSize.x * 8, y / (float)worldSize.y * 8);
+                    Vector2 v2 = new Vector2(x / (float)worldSize.x * 8, y / (float)worldSize.y * 8);
+                    Vector2 v3 = new Vector2((x + 1) / (float)worldSize.x * 8, (y + 1) / (float)worldSize.y * 8);
+                    Vector2 v4 = new Vector2(x / (float)worldSize.x * 8, (y + 1) / (float)worldSize.y * 8);*/
+                    Vector2 v1 = (((new Vector2(x, y) + Vector2.up) / worldSize) * (Vector2.one * numywumy));
+                    Vector2 v2 = (((new Vector2(x, y) + Vector2.zero) / worldSize) * (Vector2.one * numywumy));
+                    Vector2 v3 = (((new Vector2(x, y) + Vector2.one) / worldSize) * (Vector2.one * numywumy));
+                    Vector2 v4 = (((new Vector2(x, y) + Vector2.right) / worldSize) * (Vector2.one * numywumy));
+                    Vector2[] u = new Vector2[] { v1, v2, v3, v4 };
+                    
+
+                    int vertCount = vertices.Count;
+                    vertices.AddRange(v);
+                    uv.AddRange(u);
+
+                    // First triangle
+                    triangles.Add(vertCount + 0);
+                    triangles.Add(vertCount + 1);
+                    triangles.Add(vertCount + 2);
+
+                    // Second triangle
+                    triangles.Add(vertCount + 2);
+                    triangles.Add(vertCount + 1);
+                    triangles.Add(vertCount + 3);
                 }
             }
         }
@@ -162,24 +195,42 @@ public class Grid : MonoBehaviour
 
     void DrawTexture(int[,] map)
     {
-        Texture2D texture = new Texture2D(worldSize.x, worldSize.y);
-        Color[] colorMap = new Color[worldSize.x * worldSize.y];
+        Texture2D texture = new Texture2D(worldSize.x * 8, worldSize.y * 8);
+
+
+        Color[] colorMap = new Color[(worldSize.x * 8) * (worldSize.y * 8 * 8)];
+
+
         for (int x = 0; x < worldSize.x; x++)
         {
             float height = Mathf.PerlinNoise((x + seed) * terrainFreq, seed * terrainFreq) * heightMultiplier + heightAddition;
-            for (int y = 0; y < worldSize.y; y++)
+            for (int y = 0; y < worldSize.y * 8; y++)
             {
-                int cell = map[x, y];
-                if (cell == 1)
+                Texture2D tileTexture = new Texture2D(8,8);
+                Color color = Color.white;
+                if (y >= height - 1)
+                    tileTexture = GetTileTexture(Grass, 8, 24);
+                else if (y >= height - dirtLayerHeight)
+                    tileTexture = GetTileTexture(Dirt,8 ,16 );
+                else if (y <= height)
+                    tileTexture = GetTileTexture(Stone, 8,16);
+
+                Color[] tilePixels = tileTexture.GetPixels();
+                for (int i = 0; i < 8; i++)
                 {
-                    if (y >= height - 1)
-                        colorMap[y * worldSize.y + x] = Color.green;
-                    else if (y >= height - dirtLayerHeight)
-                        colorMap[y * worldSize.y + x] = new Color(0.58f,0.29f,0);
-                    else if (y <= height)
-                        colorMap[y * worldSize.y + x] = Color.gray;
+                    for (int j = 0; j < 8; j++)
+                    {
+                        float v = Random.Range(0f, 0.1f);
+                        colorMap[(((y * 8) + j) * worldSize.x * 8) + (((x * 8) + i))] = tilePixels[(7 - j) + ((7 - i) * 8)];
+
+                    }
                 }
+
+
+
             }
+
+
         }
 
         texture.filterMode = FilterMode.Point;
@@ -189,5 +240,18 @@ public class Grid : MonoBehaviour
         UnityEngine.MeshRenderer meshRenderer = gameObject.GetComponent<MeshRenderer>();
         meshRenderer.material = terrainMaterial;
         meshRenderer.material.mainTexture = texture;
+    }
+
+    Texture2D GetTileTexture(Sprite sprite, int x, int y)
+    {
+        var croppedTexture = new Texture2D(8, 8);
+
+        var pixels = sprite.texture.GetPixels(x, y, 8, 8);
+                                                
+
+        croppedTexture.SetPixels(pixels);
+        croppedTexture.Apply();
+        croppedTexture.filterMode = FilterMode.Point;
+        return croppedTexture;
     }
 }
